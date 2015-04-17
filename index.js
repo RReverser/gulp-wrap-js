6922c8663ed0f5c83acd884ce3f2f782bf63c3d5
Bogdan Chadkin <trysound@yandex.ru>
2015-04-17 23:17:01 +0300

    Fixed unhandled errors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var through = require('through2'),
 	esprima = require('esprima'),
 	estemplate = require('estemplate'),
 	escodegen = require('escodegen'),
-	applySourceMap = require('vinyl-sourcemaps-apply');
+	applySourceMap = require('vinyl-sourcemaps-apply'),
+	path = require('path');
 
 module.exports = function (tmpl, format) {
 	'use strict';
@@ -20,40 +21,42 @@ module.exports = function (tmpl, format) {
 
 		// Do nothing if no contents
 		if (file.isNull()) {
-			this.push(file);
-			return callback();
+			callback(null, file);
 		}
 
 		if (file.isStream()) {
-			this.emit('error', new gutil.PluginError('gulp-wrap-js', 'Stream content is not supported'));
-			return callback();
+			callback(new gutil.PluginError('gulp-wrap-js', 'Stream content is not supported'));
 		}
 
 		// check if file.contents is a `Buffer`
 		if (file.isBuffer()) {
-			var ast = esprima.parse(file.contents, {
-				loc: true,
-				source: file.relative,
-				range: true,
-				tokens: true,
-				comment: true
-			});
-			escodegen.attachComments(ast, ast.comments, ast.tokens);
-			ast = tmpl(ast);
-			var result = escodegen.generate(ast, {
-				comment: true,
-				format: format,
-				sourceMap: true,
-				sourceMapWithCode: true,
-				file: file.relative
-			});
-			file.contents = new Buffer(result.code);
-			if (file.sourceMap) {
-				applySourceMap(file, JSON.parse(result.map.toString()));
+			try {
+				var ast = esprima.parse(file.contents, {
+					loc: true,
+					source: file.relative,
+					range: true,
+					tokens: true,
+					comment: true
+				});
+				escodegen.attachComments(ast, ast.comments, ast.tokens);
+				ast = tmpl(ast);
+				var result = escodegen.generate(ast, {
+					comment: true,
+					format: format,
+					sourceMap: true,
+					sourceMapWithCode: true,
+					file: file.relative
+				});
+				file.contents = new Buffer(result.code);
+				if (file.sourceMap) {
+					applySourceMap(file, JSON.parse(result.map.toString()));
+				}
+				callback(null, file);
+			} catch(e) {
+				// Relative to gulpfile.js filepath with forward slashes
+				file = gutil.colors.magenta(path.relative('.', file.path).split(path.sep).join('/'));
+				callback(new gutil.PluginError('gulp-wrap-js', file + ' ' + e.message))
 			}
-			this.push(file);
 		}
-
-		return callback();
 	});
 };
